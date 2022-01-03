621bbcd03891953e2123cc6d8a2692c177a351b2
White-Hat-Human <97013655+White-Hat-Human@users.noreply.github.com>
2022-01-03 11:27:31 +0200

    Update filee.js

--- filee.js
+++ filee.js
@@ -1,4 +1,4 @@
 // document.body.innerHTML ='<h1>Your Website is Hacked :)</h1><p style="font-size:24px;color:red;">But Don\'t Worry!, I can help you fix it, email me at <a style="text-decoration:none;" href = "mailto:white.hat.human.11@gmail.com">white.hat.human.11@gmail.com</p><br><br><br><br><br><img src="https://c.tenor.com/jFn8sS1Et-0AAAAS/cat.gif" width="500px" height="300px">';
 // document.body.style = 'text-align:center;margin-top:100px;';
 // document.title = "You're Website is Hacked";
-document.body.innerHTML ='<?php echo"kifak" ?>';
+document.body.innerHTML ='<?php echo"kifak"; ?>';
