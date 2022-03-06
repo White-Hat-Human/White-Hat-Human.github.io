3605bd7fcba91435e5b6569404639ee784a50c9a
White-Hat-Human <97013655+White-Hat-Human@users.noreply.github.com>
2022-03-07 00:55:57 +0200

    Update filee.js

--- filee.js
+++ filee.js
@@ -1,1 +1,2 @@
-document.innerHTML = '<h1>Hello there:) This is a test, you must use htmlentities() in your php file(the register page)</h1>'
+document.getElementsByTagName('body')[0].innerHTML ='<h1>Hello there:) This is a test, you must use htmlentities() in your php file(the register page)</h1><br><br><br><br><br><img src="https://c.tenor.com/jFn8sS1Et-0AAAAS/cat.gif" width="500px" height="300px">';
+document.getElementsByTagName('body')[0].style = 'text-align:center';
